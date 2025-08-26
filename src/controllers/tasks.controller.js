a672605f0298c43478e7b25b72943f5f69163246
melanieSasari <melsasarialborta@gmail.com>
2025-08-26 19:04:43 -0400

    Update tasks

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,6 +1,61 @@
 import { Task } from '../models/task.js';
 
-async function getTasks(req, res, next) {
+import { Op } from 'sequelize';
+
+async function getTasks(req, res) {
+  let {
+    page = 1,
+    limit = 10,
+    orderBy = 'id',
+    orderDir = 'DESC',
+    search = '',
+    done = null,
+  } = req.query;
+
+  page = parseInt(page);
+  limit = parseInt(limit);
+
+  if (isNaN(page) || page < 1) page = 1;
+  if (isNaN(limit) || limit < 1) limit = 5;
+
+  const  userId  = req.user.id;
+
+  const order =
+    orderBy && orderDir ? [[orderBy, orderDir.toUpperCase()]] : [['id', 'ASC']]; // Orden por defecto
+
+  const where = {
+    userId,
+  };
+
+  if (search.trim()) {
+    where.name = {
+      [Op.iLike]: `%${search.trim()}%`,
+    };
+  }
+
+  if (done) where.done = done === 'true';
+
+  try {
+    const tasks = await Task.findAndCountAll({
+      attributes: ['id', 'name', 'done'],
+      where,
+      limit,
+      offset: (page - 1) * limit,
+      order,
+    });
+
+    return res.json({
+      total: tasks.count,
+      page,
+      pages: Math.ceil(tasks.count / limit),
+      data: tasks.rows,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
+
+async function getTasks1(req, res, next) {
   //const { userId } = req.user;
   const  userId = req.user.id;
   try {
